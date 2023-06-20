3b43a3857251ab0177c85cb55a081c7c6a5c5107
nikita <nikita.ratkevich91@mail.ru>
2023-06-20 15:48:26 +0200

    добавил кнопку - вернуться назад для страницы оплаты календаря

--- src/pages/Calendar/CalendarPayment.js
+++ src/pages/Calendar/CalendarPayment.js
@@ -2,11 +2,13 @@ import React, {useState, useContext} from 'react';
 import './styles.css';
 import {Context} from "../../context";
 import {createPayment} from "../../api/api";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const CalendarPayment = () => {
 
   const { paymentActions } = useContext(Context);
+
+  const navigate = useNavigate();
 
   const [emailField, setEmailField] = useState('');
   const [validationFiled, setValidationField] = useState({error: false, text: 'Заполните пожалуста поле'});
@@ -33,7 +35,6 @@ const CalendarPayment = () => {
       if(value === '') {
         setValidationField({error: true, text:'Заполните пожалуста поле'})
       } else {
-
         const newTimer = setTimeout(() => {
           setValidationField({error: true, text:'Неправильно заполнено поле ( обязательные символы: \'@\' и \'.\' )'})
         }, 800)
@@ -53,6 +54,16 @@ const CalendarPayment = () => {
           </div>
         </div>
       </header>
+      <div className="gayd-payment-back">
+        <div className="wrapper">
+          <button className={"gayd-payment-back-button"} onClick={() => navigate(-1)} >
+            <svg width="45" height="6" viewBox="0 0 45 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 3L5 5.88675V0.113249L0 3ZM45 2.5L4.5 2.5V3.5L45 3.5V2.5Z" fill="#1C1C1C"/>
+            </svg>
+            Вернуться назад
+          </button>
+        </div>
+      </div>
       <div className={"gayd-payment text-center"}>
         <div className="wrapper">
           <p className={"gayd-payment-title"}>Гайд “Календарь развития ребенка”</p>
